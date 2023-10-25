0c0b292117ac477a0320f85f80498f535c610d12
Elijah Iannucci <eiannucci@allegisgroup.com>
2023-10-25 16:15:31 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './img/logo.svg';
+import { useEffect } from "react";
+import { HashRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NavMenu } from './components/NavMenu';
+import { Footer } from './components/Footer';
+import { PageRouting } from './components/PageRouting';
+import classNames from 'classnames';
+import ReactGA from 'react-ga';
+import './scss/FontFace.module.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import GlobalStyles from './scss/Global.module.scss';
+import styles from './scss/App.module.scss';
 
 function App() {
+
+  const client = new QueryClient();
+
+  useEffect(() => {
+    ReactGA.initialize('G-SSE6730X77');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+    console.log(ReactGA);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Edit <code>src/App.js</code> and save to reload.
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={client}>
+      <div className={classNames(styles['app-custom'], 'App', 'd-flex', 'flex-column', 'min-vh-100')}>
+        <Router>
+          <NavMenu />
+          <div className={GlobalStyles['content-container']}>
+            <PageRouting />
+          </div>
+          <Footer />
+        </Router>
+      </div>
+    </QueryClientProvider>
   );
 }
 
